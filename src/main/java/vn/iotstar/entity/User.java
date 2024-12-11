271815add6b1fe2dcde496c459a19fcf6e857f24
ngothanhthang <ngothanhthang2309@gmail.com>
2024-12-11 14:30:42 +0700

    s

--- src/main/java/vn/iotstar/entity/User.java
+++ src/main/java/vn/iotstar/entity/User.java
@@ -2,9 +2,14 @@ package vn.iotstar.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @AllArgsConstructor
@@ -12,10 +17,8 @@ import java.util.List;
 @Data
 @Entity
 @Table(name = "Users")
-public class User implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
+@Builder
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
@@ -30,29 +33,62 @@ public class User implements Serializable {
     @Column(name = "email", nullable = false, columnDefinition = "nvarchar(255)")
     private String email;
 
-    @Column(name = "role", nullable = false, columnDefinition = "nvarchar(50)")
-    private String role;
-
     @Column(name = "is_active", nullable = false)
     private Boolean isActive = true;
 
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "reset_password_token")
+    private String resetPasswordToken;
     
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)  // Một User có thể có nhiều Review
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)  // Một User có thể có nhiều Review
     private List<Review> reviews;  // Danh sách các Review mà người dùng đã viết
     
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)  // Một User có thể thích nhiều sản phẩm
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)  // Một User có thể thích nhiều sản phẩm
     private List<ProductLike> productLikes;  // Danh sách các sản phẩm mà người dùng đã thích
     
-    @OneToOne(mappedBy = "user", fetch = FetchType.LAZY)
+    @OneToOne(mappedBy = "user", fetch = FetchType.EAGER)
     private Cart cart;
     
  // Mối quan hệ One-to-Many với Address (mỗi user có thể có nhiều địa chỉ)
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private List<Address> addresses;
-    
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
-    private List<UserCoupon> userCoupons;
 
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> authorityList = new ArrayList<>();
+        authorityList.add(new SimpleGrantedAuthority("ROLE_"+getRole().getName().toUpperCase()));
+//        authorityList.add(new SimpleGrantedAuthority("ROLE_USER"));
+        return authorityList;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
