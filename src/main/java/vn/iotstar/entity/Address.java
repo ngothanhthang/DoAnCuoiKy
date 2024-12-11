eb3e8c14d31f9276abb654369baa04b4e04feb89
ngothanhthang <ngothanhthang2309@gmail.com>
2024-12-11 15:26:46 +0700

    f

--- src/main/java/vn/iotstar/entity/Address.java
+++ src/main/java/vn/iotstar/entity/Address.java
@@ -29,12 +29,6 @@ public class Address implements Serializable {
     @Column(name = "city", nullable = false)
     private String city;
 
-    @Column(name = "state", nullable = false)
-    private String state;
-
-    @Column(name = "postal_code", nullable = false)
-    private String postalCode;
-
     @Column(name = "country", nullable = false)
     private String country;
 
